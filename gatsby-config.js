7cadf3174f083f6dde1a4e04c89be68a0fa097ac
vojtech.tesar <vojtech.tesar@dcsoft.cz>
2021-09-26 17:42:22 +0200

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: "\u0016",
-        spaceId: "",
+        accessToken: "15rzbJT0iZ0v7R2pbP32-Lry4YTOFQTVkeBHuGl-OXA",
+        spaceId: "21wj6gzely1i",
       },
     },
     "gatsby-plugin-sass",
-    "gatsby-plugin-image",
+    "gatsby-image",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
@@ -28,6 +28,30 @@ module.exports = {
         path: "./src/images/",
       },
       __key: "images",
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locales`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locale`,
+        languages: [`en`, `cs`],
+        defaultLanguage: `cs`,
+        siteUrl: `http://localhost:8000/`,
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false,
+          },
+          keySeparator: false,
+          nsSeparator: false,
+        },
+      },
     },
   ],
 };
